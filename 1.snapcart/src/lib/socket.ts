8dc189790332e49d91bb32c238f179bcad092a23
Sreenath.TS <sreenath.ts@ivldsp.com>
2026-04-14 00:47:15 +0530

    new change in code

--- 1.snapcart/src/lib/socket.ts
+++ 1.snapcart/src/lib/socket.ts
@@ -4,7 +4,7 @@ let socket: Socket | null = null
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    const socketUrl = "http://localhost:5001"
+    const socketUrl = process.env.NEXT_PUBLIC_SOCKET_SERVER || "http://localhost:5001"
 
     console.log("Connecting socket to:", socketUrl)
 
